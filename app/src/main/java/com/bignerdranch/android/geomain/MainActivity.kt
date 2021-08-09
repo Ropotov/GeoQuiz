ee59f4dcdbca283b7fbc28f8833992fdeacf325f
Nick <Ropotov.dev@gmail.com>
2021-08-09 20:04:33 +0400

    Added a block of buttons after a one-time answer to a question.

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.bignerdranch.android.geomain
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
+
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
+
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
@@ -28,7 +34,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
         setContentView(R.layout.activity_main)
+
+
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -38,29 +47,65 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener {
             checkAnswer(true)
+            trueButton.isClickable = false
+            falseButton.isClickable = false
 
 
         }
         falseButton.setOnClickListener {
             checkAnswer(false)
+            trueButton.isClickable = false
+            falseButton.isClickable = false
 
         }
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            if (currentIndex < (questionBank.size - 1)) {
+                currentIndex = (currentIndex + 1) % questionBank.size
+                updateQuestion()
+                trueButton.isClickable = true
+                falseButton.isClickable = true
+
+            }
 
         }
         prevButton.setOnClickListener {
             if (currentIndex > 0) {
                 currentIndex = (currentIndex - 1) % questionBank.size
                 updateQuestion()
+
             }
         }
         questionTextView.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
+
         }
         updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
     }
 
     private fun updateQuestion() {
@@ -77,5 +122,6 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+
     }
 }
